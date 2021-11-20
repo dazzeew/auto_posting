3a86da14c8631d73fe0662147cba52a3a0253796
dazzeew <56045286+dazzeew@users.noreply.github.com>
2021-11-20 22:08:33 +0700

    Update config.py

--- Готовая версия без перезапуска/config.py
+++ Готовая версия без перезапуска/config.py
@@ -1,7 +1,4 @@
-TOKEN = "2127226370:AAHFcgZSWbxltWTHtLR3FUPTzgPXPQCNJhg"
-id_admin = 1003333548
-id_chat = -1001780558467
-defolt_time_wait = 60*30
-
-#422830741 id вовы
-#1003333548 id мой
+TOKEN = "Your_token"
+id_admin = admin id
+id_chat = channel id
+defolt_time_wait = time afk
